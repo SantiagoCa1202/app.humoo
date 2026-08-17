262de067c2991c10cd6ef0f5c41fa51d6fbe7e66
Santiago Castillo <santiagoca1202@gmail.com>
2026-08-17 13:48:29 -0400

    Export purchasing receiving helpers

--- apps/client/src/features/purchasing/index.ts
+++ apps/client/src/features/purchasing/index.ts
@@ -1,3 +1,4 @@
 export * from "@/features/purchasing/forms";
 export * from "@/features/purchasing/presentation";
+export * from "@/features/purchasing/receiving";
 export * from "@/features/purchasing/types";
